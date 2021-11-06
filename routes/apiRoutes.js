0eda319b422e545f4bb0087617d7d252ad7fe6cb
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-06 14:25:49 -0400

    set up GET route for getting notes from db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,16 @@ const router = require('express').Router();
 const {notes} = require('../db/db');
 
 // GET "/api/notes" responds with all notes from the database
-
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
 
 // Post "/api/notes" responds with adding a note to database
-
+router.post('/notes', (req, res) => {
+    let newNote = req.body;
+    res.json(newNote);
+    
+})
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 
