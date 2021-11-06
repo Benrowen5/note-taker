18c4fc12bebeb633e8656066fbfb9a428f8989d4
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-06 14:51:01 -0400

    Created POST route to save a new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,12 @@
 //Requires
-const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
 const {notes} = require('../db/db');
+
+const router = require('express').Router();
+
+// const {saveNote} = require('../public/assets/js/index');
 
 // GET "/api/notes" responds with all notes from the database
 router.get('/notes', (req, res) => {
@@ -9,9 +15,14 @@ router.get('/notes', (req, res) => {
 
 // Post "/api/notes" responds with adding a note to database
 router.post('/notes', (req, res) => {
-    let newNote = req.body;
+// set new note's id based on the current length of notes array
+    req.body.id = notes.length.toString();
+    // create a new const for the note object
+    const newNote = req.body;
+    // add new note to notes array
+    notes.push(newNote);
+
     res.json(newNote);
-    
 })
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
