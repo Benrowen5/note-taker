bff4b0afc80d5d92288aae01a1c41277fcf301c3
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-10 12:24:17 -0500

    updated html routes for heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,6 @@
 const path = require('path');
 const router = require('express').Router();
 
-// GET route to serve the index.html page
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 // GET route to serve the notes.html page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
